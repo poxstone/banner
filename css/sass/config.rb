12f8da85a72b6ac9671f56a723cc583401973ab2
Oscar Ortiz <oscar.ortiz@eforcers.com>
2015-04-22 08:00:29 -0500

    javascript arma html

--- css/sass/config.rb
+++ css/sass/config.rb
@@ -8,6 +8,7 @@ images_dir = "../../imgs/"
 javascripts_dir = "../../js/"
 
 # You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
---
+++ xml.php
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8" ?> 
+<Module>
+  <ModulePrefs title="Preferences for __UP_myname__" height="400"/> 
+  <UserPref name="myname" display_name="Name" default_value="Rowan"/>
+   <Content type="html"><![CDATA[
+	   	<?php include('body.html');?>
+	   <style>
+	   <?php include('css/portal_general.css');?>
+	   </style>
+   ]]>  
+  </Content>
+</Module>
